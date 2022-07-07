797c7e3701aa49ccaedf3062a0a0182f3cd8519a
zcaiobs <zcaiobernardodasilva@gmail.com>
2022-07-07 15:28:00 -0300

    first commit

---
+++ src/main/java/com/example/oauthpost/client/UserType.java
@@ -0,0 +1,5 @@
+package com.example.oauthpost.client;
+
+public enum UserType {
+	ADMIN, CLIENT;
+}
---
+++ src/main/java/com/example/oauthpost/domain/PostRepository.java
@@ -0,0 +1,7 @@
+package com.example.oauthpost.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
